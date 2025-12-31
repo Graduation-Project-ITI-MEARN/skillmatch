8985608cc348e579ea3aedb54b3b16847881ff5f
henkshh <ahmedhassanhenkshh@gmail.com>
2025-12-31 18:30:13 +0200

    feat: connected challenges grid and details page to real api and updated seed data

--- backend/src/routes/uploadRoutes.ts
+++ backend/src/routes/uploadRoutes.ts
@@ -2,17 +2,30 @@ import auth from "../middlewares/authMiddleware";
 import express from "express";
 import { uploadDocument } from "../middlewares/upload";
 import { uploadFile } from "../controllers/uploadController";
+<<<<<<< HEAD
+=======
+import validate from "../middlewares/validate";
+import { videoLinkSchema } from "../DTO/VideoLinkDTO";
+>>>>>>> 8d2630a (chore: added dtos for moderation/payment, validated routes, and updated postman)
 
 const router = express.Router();
 
 // Route: /api/upload/file
 // 1. Check Auth -> 2. Process File -> 3. Return Response
 router.post(
+<<<<<<< HEAD
    "/file",
    // validate(videoLinkSchema),
    auth,
    uploadDocument.single("file"),
    uploadFile
+=======
+  "/file",
+  validate(videoLinkSchema),
+  auth,
+  uploadDocument.single("file"),
+  uploadFile
+>>>>>>> 8d2630a (chore: added dtos for moderation/payment, validated routes, and updated postman)
 );
 
 export default router;
