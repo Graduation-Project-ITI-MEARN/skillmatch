fb278d5007fbbabade9bd55129fdfa8bda936983
Aya Zayed <ayazayedofficial@gmail.com>
2026-01-06 03:06:43 +0200

    fix: angular prod environment

---
+++ frontend/angular/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true, // Placeholder
+  apiUrl: 'https://skillmatch-backend-production-be8a.up.railway.app/api', // Placeholder
+  nextJsUrl: 'https://skillmatch-production-545e.up.railway.app/', // Placeholder
+};
